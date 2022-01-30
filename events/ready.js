da7f61e9362778977366f30ef60da57e4e242329
hosain0489 <hosain0489@gmail.com>
2022-01-30 13:57:20 +0330

    123

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Music", {
-    type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+  await client.user.setActivity(".help", {
+    type: "STREAMING", //can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
